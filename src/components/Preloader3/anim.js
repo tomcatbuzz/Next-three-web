08f88135a3fc17638465eaafeb963980dadf6340
Tony <tomcatbuzz@yahoo.com>
2024-07-11 01:37:42 -0400

    still iw loader

--- src/components/Preloader3/anim.js
+++ src/components/Preloader3/anim.js
@@ -1,9 +1,45 @@
 export const opacity = {
   initial: {
-      opacity: 0
+      opacity: 1
   },
-  enter: {
+  exit: {
       opacity: 0.75,
       transition: {duration: 1, delay: 0.2}
   },
+}
+
+
+export const barStyles = {
+  initial: {
+    // opacity: 1
+    width: "0%"
+  }, 
+  exit: {
+    opacity: 0,
+    transition: {duration: 6, delay: 0.2}
+  },
+  // start: {
+  //   width: '10%',
+  //   transition: {
+  //     duration: 3, ease: 'easeInOut'
+  //   }
+  // },
+  // middle: {
+  //   width: '50%',
+  //   transition: {
+  //     duration: 3.2, ease: 'easeInOut'
+  //   }
+  // },
+  // end: {
+  //   width: '80%', 
+  //   transition: {
+  //     duration: 3.6, ease: 'easeInOut'
+  //   }
+  // }
+  // indeterminate: {
+  //   width: ['5%', '20%', '80%'],
+  //   transition: {
+  //     duration: 5.5, delay: 0.5,  ease: 'easeInOut'
+  //   }
+  // }
 }
--- src/components/Preloader3/index.js
+++ src/components/Preloader3/index.js
@@ -2,45 +2,58 @@ import React, { useState, useEffect } from 'react';
 import { motion, useAnimation } from 'framer-motion';
 import styles from './style.module.scss'
 import Counter from '../Counter';
+import { opacity, barStyles } from './anim';
 
 const Preloader = () => {
   const [isVisible, setIsVisible] = useState(true);
   const [animationState, setAnimationState] = useState('initial');
+  const [progress, setProgress] = useState(0);
+  const controls = useAnimation();
+
+  useEffect(() => {
+    const animateProgress = async () => {
+      controls.set({ width: '10%', transition: { duration: 4 } });
+      await controls.start({ width: '50%', transition: { duration: 2}})
+      controls.stop({ width: '80%', transition: { duration: 4 } });
+    };
+
+    animateProgress();
+  }, [controls]);
 
   useEffect(() => {
     // const timer1 = setTimeout(() => setAnimationState('digits'), 100);
     // const timer2 = setTimeout(() => setAnimationState('progress30'), 7000);
-    const timer2 = setTimeout(() => setAnimationState('progress30'), 2500);
-    const timer3 = setTimeout(() => setAnimationState('progress100'), 5500);
+    // const timer2 = setTimeout(() => setAnimationState('progress30'), 2500);
+    // const timer3 = setTimeout(() => setAnimationState('progress100'), 5500);
     // const timer4 = setTimeout(() => setIsVisible(false), 6700);
 
     return () => {
       // clearTimeout(timer1);
-      clearTimeout(timer2);
-      clearTimeout(timer3);
+      // clearTimeout(timer2);
+      // clearTimeout(timer3);
       // clearTimeout(timer4);
     };
   }, []);
 
-  // const containerVariants = {
-  //   visible: { opacity: 1, display: 'flex' },
-  //   hidden: { opacity: 0, display: 'none', transition: { duration: 2.5 } },
-  // };
-
-  const progressVariants = {
-    initial: { width: 0 },
-    progress30: { width: '30%', transition: { duration: 2 } },
-    progress100: { width: '100%', opacity: 0, transition: { duration: 2 } },
+  const containerVariants = {
+    visible: { opacity: 1, display: 'flex' },
+    hidden: { opacity: 0, display: 'none', transition: { duration: 2.5 } },
   };
+
+  // const progressVariants = {
+  //   initial: { width: 0 },
+  //   progress30: { width: '30%', transition: { duration: 2 } },
+  //   progress100: { width: '100%', opacity: 0, transition: { duration: 2 } },
+  // };
 
   return (
     <>
     <div className={styles.wrapper}>
     <motion.div 
       className={styles['pre-loader']}
-      // variants={containerVariants}
-      // initial="visible"
-      // animate={isVisible ? "visible" : "hidden"}
+      variants={containerVariants}
+      initial="visible"
+      animate={isVisible ? "visible" : "hidden"}
     >
       <p>Loading</p>
       <div className={styles.counterWrapper}>
@@ -52,9 +65,14 @@ const Preloader = () => {
       </div>
       <motion.div 
         className={styles['progress-bar']}
-        variants={progressVariants}
+        // variants={progressVariants}
+        // variants={opacity}
+        variants={barStyles}
         initial="initial"
-        animate={animationState}
+        exit="exit"
+        animate={controls}
+        // transition={{ duration: 2 }}
+        // animate={["start", "middle", "end"]}
       />
     </motion.div>
     </div>
