6a6d755cf145090ee898b837027d9056b2f8c2ef
Tony <tomcatbuzz@yahoo.com>
2024-07-13 23:56:38 -0400

    wip background animation at contact page testing

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -41,12 +41,26 @@ export default function Contact({isCanvasVisible}) {
         whileHover={{color: 'red', cursor: 'pointer'}}>
       <h1 className={styles.contactTag}>Contact page</h1>
       </motion.div> */}
+      <>
+      <div className={styles.myDiv}></div>
+      <div className={styles.myDiv2}></div>
+      <div className={styles.myDiv3}></div>
+      <div className={styles.myDiv4}></div>
+      <div className={styles.myDiv5}></div>
+      <div className={styles.myDiv6}></div>
+      <div className={styles.myDiv7}>
+      <div style={{
+        backgroundImage: 'url("data:image/svg+xml,%3csvg xmlns=\'http://www.w3.org/2000/svg\' viewBox=\'0 0 32 32\' width=\'32\' height=\'32\' fill=\'none\' stroke-width=\'2\' stroke=\'rgb(30 58 138 / 0.5)\'%3e%3cpath d=\'M0 .5H31.5V32\'/%3e%3c/svg%3e")'
+      }} className={styles.svg}></div>
+      <div className={styles.myDiv8}></div>
+      </div>
+      
       <Suspense fallback={null}>
       {/* <CubeScene /> */}
       {isCanvasVisible && 
         <Canvas
           camera={{ position: [0, 0, 20] }}
-          style={{ width: '100vw', height: '100vh', zIndex: -1 }}>
+          style={{ width: '100vw', height: '100vh', zIndex: 11, cursor: 'pointer' }}>
           <ambientLight />
           <pointLight position={[10, 10, 10]} />
           
@@ -55,6 +69,7 @@ export default function Contact({isCanvasVisible}) {
         </Canvas>
       }
       </Suspense>
+      </>
     </Page>
   )
 }
