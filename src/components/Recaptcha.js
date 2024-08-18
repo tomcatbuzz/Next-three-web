2e9a6240615d00e8260b0f26e30bfbfbd5ba5a02
Tony <tomcatbuzz@yahoo.com>
2024-08-17 21:56:11 -0400

    working errors still on recaptcha

--- src/components/Recaptcha.js
+++ src/components/Recaptcha.js
@@ -22,9 +22,9 @@ const Recaptcha = ({ onVerify }) => {
           //   }
           // );
           const response = await axios({
-            method: 'post',
+            method: "POST",
             url: RECAPTCHA_VERIFY_URL,
-            data: JSON.stringify({ token }),
+            data: {token},
             headers: {
               'Content-Type': 'application/json'
             }
@@ -32,7 +32,20 @@ const Recaptcha = ({ onVerify }) => {
           onVerify(response.data); 
         } catch (error) {
           console.error('recaptcha verification failed:', error);
-          onVerify({ error})
+          if (error.response) {
+            // The request was made and the server responded with a status code
+            // that falls out of the range of 2xx
+            console.error('Response data:', error.response.data);
+            console.error('Response status:', error.response.status);
+            console.error('Response headers:', error.response.headers);
+          } else if (error.request) {
+            // The request was made but no response was received
+            console.error('No response received:', error.request);
+          } else {
+            // Something happened in setting up the request that triggered an Error
+            console.error('Error', error.message);
+          }
+          onVerify({ error: 'verification failed'})
         }
         
       }
